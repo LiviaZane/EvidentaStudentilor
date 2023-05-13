cd8d8601e2f45783b0e84313d6f0f66c934e51c2
LiviaZane <101073713+LiviaZane@users.noreply.github.com>
2023-05-13 16:26:53 +0300

    IoC

--- Software Application/EvidentaStudentilor/EvidentaStudentilor/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-function person(str) {
-    alert(`Your personal detalis are: \n
-             ${str}.`);
-}
-
-// Write your JavaScript code.
